1d94c64086ca98053cb42ddfb308ab8ad9641f25
Paul Murray <pmurray@bigpond.com>
2026-07-10 17:26:09 +1000

    Torchbearer

---
+++ Torchbearer/js/app.js
@@ -0,0 +1,252 @@
+/* © Paul Murray 2026 https://github.com/PaulMurrayCbr/Torchbearer */
+
+import {BehaviorSubject, fromEvent, of, Subject, switchAll} from "https://esm.sh/rxjs";
+import {Torch} from "./torch.js";
+
+export class AppState {
+
+    static PAUSED = new AppState("PAUSED");
+    static RUNNING = new AppState("RUNNING");
+
+    /**
+     * @param {string} name
+     */
+    constructor(name) {
+        this.name = name;
+    }
+
+    isPaused() {
+        return this === AppState.PAUSED;
+    }
+
+    isRunning() {
+        return this === AppState.RUNNING;
+    }
+
+    toggle() {
+        return this === AppState.PAUSED ? AppState.RUNNING : AppState.PAUSED;
+    }
+
+    toString() {
+        return this.name;
+    }
+
+}
+
+export class Illumination {
+    /**
+     * @param {boolean} dark
+     * @param {number} percent
+     */
+    constructor(dark, percent) {
+        this.dark = dark;
+        if (percent < 0) percent = 0;
+        if (percent > 100) percent = 100;
+        this.percent = percent;
+    }
+}
+
+export class App {
+    /**
+     * @type {[Torch]}
+     */
+    torches = [];
+
+    selectedTorch$ = new BehaviorSubject(null);
+
+    selectedIllumination$ = new BehaviorSubject(of(null));
+
+    appState = AppState.RUNNING;
+    appState$ = new BehaviorSubject(this.appState);
+    illumination$ = new BehaviorSubject(new Illumination(true, 0));
+
+    timePasses$ = new Subject();
+    timeMark = new Date();
+
+    timeMenuOpen = false
+
+    /**
+     * @param {HTMLElement} element
+     */
+
+    constructor(element) {
+        this.element = element;
+    }
+
+    start() {
+        fromEvent(this.element.querySelector("#pause"), "click")
+            .subscribe(() => {
+                this.markTime();
+                this.appState = this.appState.toggle();
+
+                if (this.appState.isPaused()) {
+                    this.element.querySelector("#pause").classList.add("on");
+                    this.element.querySelector("#paused").classList.remove("hidden");
+                    this.selectedTorch$.next(null);
+                } else {
+                    this.element.querySelector("#pause").classList.remove("on");
+                    this.element.querySelector("#paused").classList.add("hidden");
+                }
+
+                this.appState$.next(this.appState);
+            });
+        fromEvent(this.element.querySelector("#addTorch"), "click")
+            .subscribe(() => {
+                this.addTorch();
+            });
+        fromEvent(this.element.querySelector("#close-panel"), "click")
+            .subscribe(() => {
+                this.selectTorch(null);
+            });
+        fromEvent(this.element.querySelector("#discard-torch"), "click")
+            .subscribe(() => {
+                if (this.selectedTorch$.getValue()) {
+                    this.removeTorch(this.selectedTorch$.getValue());
+                }
+            });
+        fromEvent(this.element.querySelector("#recharge-torch"), "click")
+            .subscribe(() => {
+                if (this.selectedTorch$.getValue()) {
+                    this.selectedTorch$.getValue().recharge();
+                }
+            });
+
+        fromEvent(this.element.querySelector("#time-passes"), "click")
+            .subscribe(() => {
+                this.timeMenuOpen = !this.timeMenuOpen;
+                if(this.timeMenuOpen) {
+                    this.element.querySelector("#time-passes").classList.add("on");
+                    this.element.querySelector("#time-passes-container").classList.add("open");
+                } else {
+                    this.element.querySelector("#time-passes").classList.remove("on");
+                    this.element.querySelector("#time-passes-container").classList.remove("open");
+                }
+            });
+
+        this.element.querySelectorAll(".set-minutes").forEach(button => {
+            fromEvent(button, "click").subscribe(() => {
+                if (this.selectedTorch$.getValue()) {
+                    const min = Number(button.dataset.min);
+                    this.selectedTorch$.getValue().setMaxMinutes(min);
+                }
+            });
+        });
+
+
+        this.element.querySelectorAll(".minutes-pass").forEach(button => {
+            fromEvent(button, "click").subscribe(() => {
+                const min = Number(button.dataset.min);
+                this.markTime();
+                this.timePasses$.next(min);
+            });
+        });
+
+        this.checkTorchState();
+
+        this.illumination$.subscribe(illumination => {
+            if (illumination.dark) {
+                this.element.querySelector("#darkness").classList.add("visible");
+            } else {
+                this.element.querySelector("#darkness").classList.remove("visible");
+            }
+
+            document.documentElement.style.setProperty(
+                "--brightness",
+                Math.trunc(20 + illumination.percent * .6).toString()
+            );
+
+        });
+
+        this.selectedTorch$.subscribe(torch => {
+            if (torch) {
+                this.element.querySelector("#panel-container").classList.add("open");
+                this.selectedIllumination$.next(torch.state$);
+            } else {
+                this.element.querySelector("#panel-container").classList.remove("open");
+                this.selectedIllumination$.next(of(null));
+            }
+        })
+
+        this.selectedIllumination$
+            .pipe(
+                switchAll()
+            )
+            .subscribe(illumination => {
+                console.log("Selected illumination", illumination);
+
+                if (illumination) {
+                    this.element.querySelector("#time-remaining").textContent = illumination.getTimeDisplay();
+                } else {
+                    // this almost never happens
+                    this.element.querySelector("#time-remaining").textContent = "No selection";
+                }
+            })
+
+        const timer = setInterval(() => {
+            this.markTime();
+        }, 10000);
+
+    }
+
+    addTorch() {
+        const template = document.getElementById("torch-template");
+        const clone = template.content.cloneNode(true);
+        const element = clone.firstElementChild;
+        document.getElementById("torches-grid").appendChild(element);
+        const torch = new Torch(this, element);
+        this.torches.push(torch);
+        this.markTime();
+        torch.start();
+
+        // I'll just jam this subscription into the torch object
+        torch.appSubscription = torch.state$.subscribe(state => {
+            this.checkTorchState();
+        })
+    }
+
+    /**
+     * @param {Torch} torch
+     */
+    removeTorch(torch) {
+        if (this.selectedTorch$.getValue() == torch) {
+            this.selectedTorch$.next(null);
+        }
+        torch.appSubscription.unsubscribe();
+        torch.stop();
+        torch.element.remove();
+        this.torches = this.torches.filter(t => t !== torch);
+        this.checkTorchState();
+    }
+
+    /**
+     * @param {Torch} torch
+     */
+    selectTorch(torch) {
+        this.selectedTorch$.next(torch);
+    }
+
+    checkTorchState() {
+        /** @type {TorchState[]} */
+        const state = this.torches.map(torch => torch.state$.getValue());
+
+        let dark = true;
+        let remainingPercent = 0;
+        for (const i of state) {
+            if (i.ignited && i.remainingPercent > 0) {
+                dark = false;
+                remainingPercent = Math.max(remainingPercent, i.remainingPercent);
+            }
+        }
+
+        this.illumination$.next(new Illumination(dark, remainingPercent));
+    }
+
+    markTime() {
+        const now = new Date();
+        if (this.appState.isRunning()) {
+            const diff = now.getTime() - this.timeMark.getTime();
+            this.timePasses$.next(diff / 1000 / 60); // minutes
+        }
+        this.timeMark = now;
+    }
+}
---
+++ Torchbearer/js/torch.js
@@ -0,0 +1,151 @@
+/* © Paul Murray 2026 https://github.com/PaulMurrayCbr/Torchbearer */
+
+import {BehaviorSubject, Subject, takeUntil} from "https://esm.sh/rxjs";
+
+import {App} from "./app.js";
+
+export class TorchState {
+    constructor(ignited, maxMinutes, minutesRemaining) {
+        this.maxMinutes = maxMinutes;
+        this.minutesRemaining = minutesRemaining;
+        this.ignited = ignited;
+        this.remainingPercent = this.minutesRemaining / this.maxMinutes * 100;
+    }
+
+    getTimeDisplay() {
+        if (this.minutesRemaining <= 0) {
+            return `Torch consumed. Recharge for ${this.maxMinutes} minute${this.maxMinutes>1?'s':''}`;
+        }
+
+        const blocks = Math.floor((this.minutesRemaining + 7.25) / 15);
+
+        if (blocks <= 0) {
+            return `${this.maxMinutes} minute${this.maxMinutes>1?'s':''} almost done`;
+        }
+
+        return `About ${blocks * 15} minutes remaining of ${this.maxMinutes}`;
+    }
+}
+
+export class Torch {
+
+    destroy$ = new Subject();
+
+    ignited = false;
+    maxMinutes = 1;
+    minutesRemaining = 1;
+
+    state$ = new BehaviorSubject(new TorchState(this.ignited, this.maxMinutes, this.minutesRemaining));
+
+    /**
+     * @param {App} app
+     * @param {HTMLElement} element
+     */
+    constructor(app, element) {
+        this.app = app;
+        this.element = element;
+
+        this.torch0 = element.querySelector(".torch0");
+        this.torch1 = element.querySelector(".torch1");
+        this.torch2 = element.querySelector(".torch2");
+        this.torch3 = element.querySelector(".torch3");
+    }
+
+    emitState() {
+        this.state$.next(new TorchState(this.ignited, this.maxMinutes, this.minutesRemaining));
+    }
+
+    start() {
+        this.element.addEventListener("pointerup", e => {
+            if (this.app.selectedTorch$.getValue() != this) {
+                this.app.selectTorch(this);
+            } else {
+                this.ignited = !this.ignited;
+                this.update();
+            }
+        });
+
+        this.element.addEventListener("pointermove", e => {
+            if (this.app.selectedTorch$.getValue() != this) {
+                this.app.selectTorch(this);
+            }
+        });
+
+        this.app.selectedTorch$
+            .pipe(
+                takeUntil(this.destroy$)
+            )
+            .subscribe(selected => {
+                if (selected === this) {
+                    this.element.classList.add("selected");
+                } else {
+                    this.element.classList.remove("selected");
+                }
+            });
+
+        this.app.timePasses$.pipe(takeUntil(this.destroy$)).subscribe(minutes => {
+            if (this.ignited) {
+                this.minutesRemaining -= minutes;
+
+                if(this.minutesRemaining <= 0) {
+                    this.ignited = false;
+                    this.minutesRemaining = 0;
+                }
+
+                this.update();
+            }
+        });
+
+        this.update();
+    }
+
+    stop() {
+        this.state$.complete();
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
+    recharge() {
+        this.minutesRemaining = this.maxMinutes;
+        this.update();
+    }
+
+    setMaxMinutes(minutes) {
+        this.maxMinutes = minutes;
+        this.update();
+    }
+
+    update() {
+        this.recheckOpacity();
+        this.emitState()
+    }
+
+    recheckOpacity() {
+        const remainingPercent = (this.minutesRemaining / this.maxMinutes) * 100;
+
+        if (this.ignited && remainingPercent > 0) {
+            this.torch0.style.opacity = 0;
+            this.torch1.style.opacity = 0;
+            this.torch2.style.opacity = 0;
+            this.torch3.style.opacity = 0;
+
+            if (remainingPercent > 50) {
+                this.torch2.style.opacity = 1;
+                this.torch3.style.opacity = (remainingPercent - 50) / 50;
+            } else if (remainingPercent > 25) {
+                this.torch1.style.opacity = 1;
+                this.torch2.style.opacity = (remainingPercent - 25) / 25;
+            } else {
+                this.torch0.style.opacity = 1;
+                this.torch1.style.opacity = remainingPercent / 25;
+            }
+        } else {
+            this.torch0.style.opacity = 1;
+            this.torch1.style.opacity = 0;
+            this.torch2.style.opacity = 0;
+            this.torch3.style.opacity = 0;
+        }
+
+    }
+
+}
